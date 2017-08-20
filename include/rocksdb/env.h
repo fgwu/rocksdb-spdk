62f2c92dd621748d52918351de4eebd90dab673c
Fenggang Wu <fenggang@cs.umn.edu>
2017-08-20 16:32:22 -0500

    Update with SPDK BlobStore API Change NewSpdkEnv()

--- include/rocksdb/env.h
+++ include/rocksdb/env.h
@@ -1092,7 +1092,7 @@ Env* NewMemEnv(Env* base_env);
 Status NewHdfsEnv(Env** hdfs_env, const std::string& fsname);
 
 // Returns a new environment that is used for SPDK environment.
-void NewSpdkEnv(Env** spdk_env, const std::string& fsname, const std::string& confname,
+Env* NewSpdkEnv(Env* base_env, const std::string& fsname, const std::string& confname,
 		const std::string& bdevname, uint64_t cache_size_in_mb);
 
 // Initializes a thread for SpdkEnv processing.
